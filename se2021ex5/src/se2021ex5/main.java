847a5292d5b4b8b3a33d46ea817fee08509ea5f7
MaoIzumi <r202003443gz@jindai.jp>
2022-01-29 10:33:50 +0900

    This is version3

--- se2021ex5/src/se2021ex5/main.java
+++ se2021ex5/src/se2021ex5/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("version2 se2021ex5");
+System.out.println("version3 se2021ex5");
 	}
 
 }
